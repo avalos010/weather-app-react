7573ba311a0567379f33acf2be3115fad454f95b
avalos010 <luizavalos40@gmail.com>
2019-05-06 12:16:58 -0500

    Updates

---
+++ precache-manifest.928cdb4b77557aa09bbe59d15d3148eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cf1785d29e698614deaf90fc6aa04e",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "d2825ae81dac39d4ca24",
+    "url": "/weather-app-react/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "79085f666d2434854a72",
+    "url": "/weather-app-react/static/css/main.f77cad2d.chunk.css"
+  },
+  {
+    "revision": "d2825ae81dac39d4ca24",
+    "url": "/weather-app-react/static/js/2.aa8a1e70.chunk.js"
+  },
+  {
+    "revision": "79085f666d2434854a72",
+    "url": "/weather-app-react/static/js/main.f405244b.chunk.js"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  }
+]);
